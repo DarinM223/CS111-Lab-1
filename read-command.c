7bcb5a64f6339b9fcc545161efd55bedaf2fb148
Darin Minamoto <darinm223@gmail.com>
2014-01-10 07:45:44 -0800

    Implemented read_command_stream and initialized stream in make_command_stream

--- read-command.c
+++ read-command.c
@@ -2,30 +2,47 @@
 
 #include "command.h"
 #include "command-internals.h"
-
+#include "alloc.h"
 #include <error.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <error.h>
+#include <errno.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+struct command_stream {
+        command_t* commands; /*array of commands*/
+        int commandNum; /*number of commands*/
+        int index; /*current index of commands*/
+};
 
+#define commands_size 10
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
+  /*initialize stream*/
+  command_stream_t stream = (command_stream_t) checked_malloc(sizeof(struct command_stream)); /*allocate stream size*/
+  stream->commands = (command_t*) checked_malloc(commands_size*sizeof(command_t)); /*allocate commands with arbitrary command size (should be "robust")*/
+  stream->commandNum = 0;
+  stream->index = 0;
   return 0;
 }
 
 command_t
 read_command_stream (command_stream_t s)
 {
-  /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+  command_t comm;
+  /* if s is NULL or you are at end of stream return NULL */
+  if ((s == NULL) || (s->index >= s->commandNum)) { return NULL; }
+  /*set the command to the command at the current index of the command stream*/
+  comm = s->commands[s->index];
+  s->index++; /*increment current index counter*/
+  return comm;
 }
