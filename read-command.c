2ec42615f1f46eb93d74d58afd0f75e892c71ad2
Darin Minamoto <darinm223@gmail.com>
2014-01-11 09:50:49 -0800

    Added pipeline and subsequence operator and abstracted stuff to functions

--- read-command.c
+++ read-command.c
@@ -26,22 +26,33 @@ int isValidWordChar(char c) { /*checks if character is valid word character*/
     return (isascii(c) && (isalum(c) || strchr("!%+,-./:@^_",c)));
 }
 
+/* returns 0 if an operator command was created, 1 if the only thing that happened was the command was pushed on the stack*/
+int addSimpleCommand(char *str) {
+      /*push str to command stack*/
+      command_t c = createSimpleCommand(str);
+      addCommand(c);
+      /*peek at operator stack*/
+      if (opPeek() != -1 && commandSize() >= 2) {
+            /*create command with the current operator and 2 most current commands*/
+            command_t prevCom1 = commandPop();
+            command_t prevCom2 = commandPop();
+            command_t c = createOperatorCommand(opPop(), prevCom1, prevCom2);
+            /*push command onto command stack*/
+            addCommand(c);
+            return 0;
+      }
+      return 1;
+}
 
-#define commands_size 10
+
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
   int curByte;
   command_stream_t stream;
-  command_t currCommand;
 
-  /*initialize stream*/
-  stream = (command_stream_t) checked_malloc(sizeof(struct command_stream)); /*allocate stream size*/
-  stream->commands = (command_t*) checked_malloc(commands_size*sizeof(command_t)); /*allocate commands with arbitrary command size (should be "robust")*/
-  stream->commandNum = 0;
-  stream->index = 0;
-  /*builds the stack*/
+   /*builds the stack*/
   char prevChar = 0;
   int str_size;
   char *str;
@@ -49,8 +60,14 @@ make_command_stream (int (*get_next_byte) (void *),
   /* read every byte */
   for (curByte = get_next_byte(get_next_byte_argument); curByte != EOF ; ) {
       if (isValidWordChar(curByte)) { /*if the character is a word character*/
-          if (prevChar == '&' || prevChar == '|') { /*if the previous character was '&' or '|'*/
+          if (prevChar == '&') { /*if the previous character was '&' */
               /*print error*/
+          }
+          if (prevChar == '|') { /*if the previous character was '|' */
+               /*its a pipeline*/
+               addSimpleCommand(str);
+               addOp(PIPE_COMMAND);
+               prevChar = 0;
           }
           append(curByte, &str, &str_size); /*add the word to a temporary string (will resize if necessary)*/
           /*is there a better way to append to string??*/
@@ -58,18 +75,7 @@ make_command_stream (int (*get_next_byte) (void *),
           if (c == '&') {
               if (prevChar == '&') {
                   /*its a double and*/
-                  /*push str to command stack*/
-                  command_t c = createSimpleCommand(str);
-                  addCommand(c);
-                  /*peek at operator stack*/
-                  if (opPeek() != -1) {
-                       /*create command with the current operator and 2 most current commands*/
-                       command_t prevCom1 = commandPop();
-                       command_t prevCom2 = commandPop();
-                       command_t c = createOperatorCommand(opPop(), prevCom1, prevCom2);
-                       /*push command onto command stack*/
-                       addCommand(c);
-                  }
+                  addSimpleCommand(str);
                   /*push and to operator stack*/
                   addOp(AND_COMMAND);
                   prevChar = 0;
@@ -78,33 +84,34 @@ make_command_stream (int (*get_next_byte) (void *),
           }  else if (c == '|') {
               if (prevChar == '|') {
                   /*its a double or*/
-                  /*push str to command stack*/
-                  command_t c = createSimpleCommand(str);
-                  addCommand(c);
-                  /*peek at operator stack*/
-                  if (opPeek() != -1) {
-                       /*create command with the current operator and 2 most current commands*/
-                       command_t prevCom1 = commandPop();
-                       command_t prevCom2 = commandPop();
-                       command_t c = createOperatorCommand(opPop(), prevCom1, prevCom2);
-                       /*push command onto command stack*/
-                       addCommand(c);
-                  }
-
+                  addSimpleCommand(str);
                   /*push or to operator stack*/
                   addOp(OR_COMMAND);
                   prevChar = 0;
               } else
                   prevChar = '|';
-          } else if (prevChar == '&' || prevChar == '|') { /* if the character isn't '&' or '|' but the previous character was*/
+          } else if (prevChar == '&' || prevChar == '|')  { /* if the character isn't '&' but the previous character was*/
               /*print error*/
+          } else if (c == ';') { /* if the character is the ';' character */
+                  addSimpleCommand(str);
+                  addOp(SEQUENCE_COMMAND);
+                  prevChar = 0;
           }
       } else {
           /*print error*/
       }
   }
- 
-  return 0;
+  /*get the command tree*/
+  command_t command_tree = commandPop();
+  /*get the size of the tree to allocate*/
+  commands_size = sizeOfTree(command_tree);
+  /*initialize stream*/
+  stream = (command_stream_t) checked_malloc(sizeof(struct command_stream)); /*allocate stream size*/
+  stream->commands = (command_t*) checked_malloc(commands_size*sizeof(command_t)); /*allocate commands with arbitrary command size (should be "robust")*/
+  stream->commandNum = 0;
+  stream->index = 0;
+  /*build the stream from the command tree*/
+  return buildStream(stream, command_tree);
 }
 
 command_t
