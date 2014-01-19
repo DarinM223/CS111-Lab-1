9d3368cad361d6664d128234d872660a1a8aa7e3
Darin Minamoto <darinm223@gmail.com>
2014-01-19 00:25:44 -0800

    Started on execution stuff

--- execute-command.c
+++ execute-command.c
@@ -3,6 +3,10 @@
 #include "command.h"
 #include "command-internals.h"
 
+#include <sys/types.h>
+#include <sys/wait.h> 
+#include <unistd.h>  
+#include <stdlib.h>  
 #include <error.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
@@ -13,6 +17,20 @@ command_status (command_t c)
 {
   return c->status;
 }
+char **currentlyUsedInputFiles;
+
+void execute(command_t comm);
+void executeAnd(command_t comm);
+void executeOr(command_t comm);
+
+/*creates another process if TIMETRAVEL == 1 only if the input/output stuff is correct*/
+void executeSequence(command_t comm); 
+
+void executePipe(command_t comm); /*use the C pipe() function*/
+void executeSimple(command_t comm);
+void dealWithRedirection(command_t comm); /*use the C open() function*/
+
+int TIMETRAVEL=0; /*Time travel is a preset option before running the program so it can be global?*/
 
 void
 execute_command (command_t c, int time_travel)
@@ -20,5 +38,65 @@ execute_command (command_t c, int time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+  /*error (1, 0, "command execution not yet implemented");*/
+        TIMETRAVEL=time_travel;
+        execute(c);
 }
+void execute(command_t comm ) {
+        switch (comm->type) {
+                case AND_COMMAND:
+                    executeAnd(comm );
+                    break;  
+                case OR_COMMAND:
+                    executeOr(comm );
+                    break;
+                case SEQUENCE_COMMAND:
+                    executeSequence(comm );
+                    break;
+                case PIPE_COMMAND:
+                    executePipe(comm );
+                    break;
+                case SIMPLE_COMMAND:
+                    executeSimple(comm );                    
+                    break;
+                case SUBSHELL_COMMAND:
+                    break;
+        }
+}
+
+void executeAnd(command_t comm) {
+        /*first execute first command*/
+        execute(comm->u.command[0]);
+        if (comm->u.command[0]->status == 0) { /*if first command completed successfully*/
+                /*execute 2nd command and set status to 2nd command*/
+                execute(comm->u.command[1]);
+                comm->status = comm->u.command[1]->status;
+        } else {
+                /*otherwise set status to 1st command status*/
+                comm->status = comm->u.command[0]->status;
+        }
+}
+/*Executes or command (or command executes first command and the second one if the first one didn't return 0) */
+void executeOr(command_t comm) {
+        /*first execute first command*/
+        execute(comm->u.command[0]);
+        if (comm->u.command[0]->status != 0) { /*if first command didn't complete*/
+                /*execute 2nd command and set status to 2nd command*/
+                execute(comm->u.command[1]);
+                comm->status = comm->u.command[1]->status;
+        } else {
+                /*otherwise set status to 1st command status*/
+                comm->status = comm->u.command[0]->status;
+        }
+
+}
+void executeSequence(command_t comm ) {
+        if (TIMETRAVEL == 0) {
+                execute(comm->u.command[0]);
+                execute(comm->u.command[1]);
+                comm->status = comm->u.command[1]->status;
+        } else {
+                /* TODO: Implement timetravel stuff */
+        }
+}
+
